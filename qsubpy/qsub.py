9f86a00001e7f0806ba827f5a55e9c5e7f2b7c09
philipjhj <philipjhj@gmail.com>
2020-02-01 19:07:07 +0100

    first attempt at adding lsf hpc calls

--- qsubpy/qsub.py
+++ qsubpy/qsub.py
@@ -15,56 +15,115 @@ def get_timestamp():
     return datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
-QSUB_FILE_NAME = 'qsub_file.qsub'
+HPC_PLATFORM = 'torque'
+
 LOG_PATH = 'logs'
 
-TEMPLATE_BASE = """
-#!/bin/sh
-# embedded options to qsub - start with #PBS
-# -- Name of the job ---
-#PBS -N {job_name}
-# -- specify queue --
-#PBS -q hpc
-# -- estimated wall clock time (execution time): hh:mm:ss --
-#PBS -l walltime={walltime}
-# -- number of processors/cores/nodes --
-#PBS -l nodes={n_nodes}:ppn={ppn}
-# -- user email address --
-# please uncomment the following line and put in your e-mail address,
-# if you want to receive e-mail notifications on a non-default address
-##PBS -M your_email_address
-# -- mail notification --
-##PBS -m abe
-#PBS -o {logfile}
-#PBS -e {errfile}
-# -- run in the current working (submission) directory --
-if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
-# here follow the commands you want to execute
+HPC_FILE_NAME = None
+TEMPLATE_BASE = None
+TEMPLATE_HPC_CODE = None
+PYTHON_CLEANUP_CODE_TEMPLATE = None
 
-echo '**** Activating conda environment ****: env_name = '{env}
-source activate {env}
-"""
 
-TEMPLATE_QSUB_CODE = TEMPLATE_BASE+"""
-echo '**** Running script ****'
-{script_call}
-echo '**** Script completed ****'
-"""
+def config(hpc_platform='torque'):
+    global HPC_PLATFORM, HPC_FILE_NAME, TEMPLATE_BASE, TEMPLATE_HPC_CODE, PYTHON_CLEANUP_CODE_TEMPLATE
 
-PYTHON_CLEANUP_CODE_TEMPLATE = """
-# SCRIPT SUFFIX AUTOMATICALLY ADDED
-#import os
-#print('removing script file')
-#os.remove('{script}')
-"""
+    HPC_PLATFORM = hpc_platform
+
+    if HPC_PLATFORM == 'torque':
+
+        HPC_FILE_NAME = 'hpc_file.qsub'
+
+        TEMPLATE_BASE = """
+        #!/bin/sh
+        # embedded options to qsub - start with #PBS
+        # -- Name of the job ---
+        #PBS -N {job_name}
+        # -- specify queue --
+        #PBS -q hpc
+        # -- estimated wall clock time (execution time): hh:mm:ss --
+        #PBS -l walltime={walltime}
+        # -- number of processors/cores/nodes --
+        #PBS -l nodes={n_nodes}:ppn={ppn}
+        # -- user email address --
+        # please uncomment the following line and put in your e-mail address,
+        # if you want to receive e-mail notifications on a non-default address
+        ##PBS -M your_email_address
+        # -- mail notification --
+        ##PBS -m abe
+        #PBS -o {logfile}
+        #PBS -e {errfile}
+        # -- run in the current working (submission) directory --
+        if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
+        # here follow the commands you want to execute
+
+        echo '**** Activating conda environment ****: env_name = '{env}
+        source activate {env}
+        """
+
+        TEMPLATE_HPC_CODE = TEMPLATE_BASE+"""
+        echo '**** Running script ****'
+        {script_call}
+        echo '**** Script completed ****'
+        """
+
+        PYTHON_CLEANUP_CODE_TEMPLATE = """
+        # SCRIPT SUFFIX AUTOMATICALLY ADDED
+        #import os
+        #print('removing script file')
+        #os.remove('{script}')
+        """
+    elif HPC_PLATFORM == 'lsf':
+        HPC_FILE_NAME = 'hpc_file.bsub'
+
+        TEMPLATE_BASE = """
+        #!/bin/sh
+        # embedded options to bsub
+        # -- Name of the job ---
+        #BSUB -J {job_name}
+        # -- specify queue --
+        #BSUB -q hpc
+        # -- estimated wall clock time (execution time): hh:mm:ss --
+        #BSUB -W {walltime}
+        # -- number of processors/cores/nodes --
+        #BSUB -n {ppn}
+        #BSUB -R "span[ptile=m]"
+        #BSUB -R "span[hosts={n_nodes}]"
+        # -- user email address --
+        # please uncomment the following line and put in your e-mail address,
+        # if you want to receive e-mail notifications on a non-default address
+        ##BSUB -u your_email_address
+        # -- mail notification --
+        ##BSUB -BN 
+        #BSUB -o {logfile}
+        #BSUB -e {errfile}
+
+        # here follow the commands you want to execute
+
+        echo '**** Activating conda environment ****: env_name = '{env}
+        source activate {env}
+        """
+
+        TEMPLATE_HPC_CODE = TEMPLATE_BASE+"""
+        echo '**** Running script ****'
+        {script_call}
+        echo '**** Script completed ****'
+        """
+
+        PYTHON_CLEANUP_CODE_TEMPLATE = """
+        # SCRIPT SUFFIX AUTOMATICALLY ADDED
+        #import os
+        #print('removing script file')
+        #os.remove('{script}')
+        """
 
 
 def prepare_output_location(output_path):
-    qsub_output_path = Path(output_path) / 'qsub_files'
-    qsub_output_path.mkdir(parents=True, exist_ok=True)
-    qsub_output_path.joinpath(LOG_PATH).mkdir(parents=True, exist_ok=True)
+    hpc_output_path = Path(output_path) / 'hpc_files'
+    hpc_output_path.mkdir(parents=True, exist_ok=True)
+    hpc_output_path.joinpath(LOG_PATH).mkdir(parents=True, exist_ok=True)
 
-    return qsub_output_path
+    return hpc_output_path
 
 
 def prepare_script_from_code(code, code_cleanup_template, script_file):
@@ -77,27 +136,36 @@ def prepare_script_from_code(code, code_cleanup_template, script_file):
     open(script_file, 'w').write(script_text)
 
 
-def prepare_qsub_file(qsub_output_path, script_call, job_name, logfile, errfile, env, n_nodes=1, ppn=1, walltime='10:00:00'):
-    open(str(qsub_output_path / QSUB_FILE_NAME), 'w').write(TEMPLATE_QSUB_CODE.format(script_call=script_call,
-                                                                                      job_name=job_name,
-                                                                                      logfile=qsub_output_path/LOG_PATH/logfile,
-                                                                                      errfile=qsub_output_path/LOG_PATH/errfile,
-                                                                                      env=env,
-                                                                                      n_nodes=n_nodes,
-                                                                                      ppn=ppn,
-                                                                                      walltime=walltime))
+def prepare_hpc_file(hpc_output_path, script_call, job_name, logfile, errfile, env, n_nodes=1, ppn=1, walltime='10:00:00'):
+    if HPC_PLATFORM == 'lsf':
+        # lsf takes total number of cores
+        ppn = ppn*n_nodes
+
+    with open(str(hpc_output_path / HPC_FILE_NAME), 'w') as f:
+        f.write(TEMPLATE_HPC_CODE.format(script_call=script_call,
+                                         job_name=job_name,
+                                         logfile=hpc_output_path/LOG_PATH/logfile,
+                                         errfile=hpc_output_path/LOG_PATH/errfile,
+                                         env=env,
+                                         n_nodes=n_nodes,
+                                         ppn=ppn,
+                                         walltime=walltime))
 
 
-def submit_job(qsub_output_path):
-    subprocess.call('qsub < ' + str(qsub_output_path /
-                                    QSUB_FILE_NAME), shell=True)
+def submit_job(hpc_output_path):
+    if HPC_PLATFORM == 'torque':
+        subprocess.call('qsub < ' + str(hpc_output_path /
+                                        HPC_FILE_NAME), shell=True)
+    elif HPC_PLATFORM == 'lsf':
+        subprocess.call('bsub < ' + str(hpc_output_path /
+                                        HPC_FILE_NAME), shell=True)
 
 
 def submit_python_code(code, output_path, script_arguments='', job_name="job", logfile="$PBS_JOBID.output", errfile="$PBS_JOBID.error", env='base', n_nodes=1, ppn=1, walltime='10:00:00'):
 
-    qsub_output_path = prepare_output_location(output_path)
+    hpc_output_path = prepare_output_location(output_path)
 
-    script_file_name = qsub_output_path / 'job_script.py'
+    script_file_name = hpc_output_path / 'job_script.py'
 
     prepare_script_from_code(
         code, PYTHON_CLEANUP_CODE_TEMPLATE, script_file_name)
@@ -109,30 +177,30 @@ def submit_python_code(code, output_path, script_arguments='', job_name="job", l
 
 def submit_bash_code(code, output_path, job_name="job", logfile="$PBS_JOBID.output", errfile="$PBS_JOBID.error", env='base', n_nodes=1, ppn=1, walltime='10:00:00'):
 
-    qsub_output_path = prepare_output_location(output_path)
+    hpc_output_path = prepare_output_location(output_path)
 
     script_call = code
 
-    prepare_qsub_file(qsub_output_path, script_call,
-                      job_name, logfile, errfile, env,
-                      n_nodes, ppn, walltime)
+    prepare_hpc_file(hpc_output_path, script_call,
+                     job_name, logfile, errfile, env,
+                     n_nodes, ppn, walltime)
 
-    submit_job(qsub_output_path)
+    submit_job(hpc_output_path)
 
 
 def submit_python_script(script_file_path, output_path, script_arguments='', job_name="job", logfile="$PBS_JOBID.output", errfile="$PBS_JOBID.error", env='base', n_nodes=1, ppn=1, walltime='10:00:00'):
 
-    qsub_output_path = prepare_output_location(output_path)
+    hpc_output_path = prepare_output_location(output_path)
 
-    job_script_name = qsub_output_path / 'job_script.py'
+    job_script_name = hpc_output_path / 'job_script.py'
 
     if not job_script_name.exists():
         shutil.copy(script_file_path, job_script_name)
 
     script_call = "python " + str(job_script_name) + ' ' + script_arguments
 
-    prepare_qsub_file(qsub_output_path, script_call,
-                      job_name, logfile, errfile, env,
-                      n_nodes, ppn, walltime)
+    prepare_hpc_file(hpc_output_path, script_call,
+                     job_name, logfile, errfile, env,
+                     n_nodes, ppn, walltime)
 
-    submit_job(qsub_output_path)
+    submit_job(hpc_output_path)
